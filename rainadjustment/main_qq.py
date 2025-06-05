613c5b91d82b091d6a2c3bd2cff82fd16a8cc703
maartenverbrugge <maarten.verbrugge@deltares.nl>
2025-06-05 11:24:32 +0200

    Introduce option `leadtime_specific` to chose whether to use dimension leadtime for fitting/correction purposes

--- rainadjustment/main_qq.py
+++ rainadjustment/main_qq.py
@@ -24,7 +24,6 @@ def preprocess_FEWS_netCDF(input_ds):
     ds = ds.rename({'time': 'valid_time'})
     return ds
 
-
 def create_qmapping_factors(grid_values, obs_values):
     '''
     Past correctie toe op distributie van tijdstap observaties.
@@ -61,50 +60,63 @@ def apply_qmap_correction(forecast: float, historical_data: np.ndarray[:], cor_f
     '''
     correction = np.interp(forecast, historical_data, cor_factor)
     apply_corr = np.where(correction > 1000, 1000, correction)
-    return np.array([correction, forecast * apply_corr])
+    return np.array([correction, forecast*apply_corr])
 
 
-def Q_mapping_gridref(init_month: int, logger, preprocess_files=False):
+def Q_mapping_gridref(init_month: int, logger, preprocess_files = False, leadtime_specific = False):
+
     if preprocess_files == True:
         files = glob.glob(f'./input/historic_forecasts/*.nc')
-        grid_hist = xr.open_mfdataset(files, preprocess=preprocess_FEWS_netCDF)
+        grid_hist=xr.open_mfdataset(files, preprocess= preprocess_FEWS_netCDF)
         grid_hist.to_netcdf('./input/combined_historic_forecasts.nc')
     else:
         grid_hist = xr.load_dataset('./input/combined_historic_forecasts.nc')
-    grid_hist = check_dimensions(grid_hist, logger=logger)
+    grid_hist = check_dimensions(grid_hist, logger= logger)
 
     grid_hist['lon'] = np.round(grid_hist.lon.values, decimals=2)
     grid_hist['lat'] = np.round(grid_hist.lat.values, decimals=2)
     select_timestamps = grid_hist.analysis_time.dt.month.values == init_month
     grid_hist = grid_hist.isel({'analysis_time': select_timestamps})
-    grid_hist = grid_hist.isel({'analysis_time': grid_hist.analysis_time.dt.day.values < 27})
+    grid_hist = grid_hist.isel({'analysis_time': grid_hist.analysis_time.dt.day.values <27})
     print(grid_hist.valid_time.values)
 
     ref_hist = xr.load_dataset('./input/reference_rainfall.nc')
-    ref_hist = check_dimensions(ref_hist, logger=logger)
-    ref_hist = ref_hist.sel({'time': np.unique(grid_hist.valid_time.values.flatten())})
+    ref_hist = check_dimensions(ref_hist, logger = logger)
+    # ref_hist = ref_hist.sel({'time': np.unique(grid_hist.valid_time.values.flatten())})
     ref_hist['lon'] = np.round(ref_hist.lon.values, decimals=2)
     ref_hist['lat'] = np.round(ref_hist.lat.values, decimals=2)
 
-    q_list = []
+
     grid_hist = grid_hist.compute()
-    for step_it, leadtime in enumerate(grid_hist.step.values):
-        print(step_it)
-        grid_at_step = grid_hist.isel({'step': step_it})
-        ref_hist_step = ref_hist.sel({'time': np.unique(grid_hist.valid_time.values.flatten())})
+    if not leadtime_specific:
         factors = xr.apply_ufunc(
             create_qmapping_factors,
-            grid_at_step.P,
-            ref_hist_step.P,
-            input_core_dims=[['analysis_time'], ['time']],
+            grid_hist.P,
+            ref_hist.P,
+            input_core_dims=[['analysis_time', 'step'], ['time']],
             output_core_dims=[['variable', 'percentile']],
             vectorize=True,
         )
-        q_list.append(factors)
+        q_array = factors
+    else:
+        q_list = []
+        for step_it, leadtime in enumerate(grid_hist.step.values):
+            print(step_it)
+            grid_at_step = grid_hist.isel({'step': step_it})
+            #ref_hist_step = ref_hist.sel({'time': np.unique(grid_hist.valid_time.values.flatten())})
+            factors = xr.apply_ufunc(
+                create_qmapping_factors,
+                grid_at_step.P,
+                ref_hist.P,
+                input_core_dims=[['analysis_time'], ['time']],
+                output_core_dims=[['variable', 'percentile']],
+                vectorize=True,
+            )
+            q_list.append(factors)
+        q_array = xr.concat(q_list, dim = 'step')
 
-    q_array = xr.concat(q_list, dim='step')
-    q_array = q_array.assign_coords(percentile=np.linspace(0, 1, 201))
-    q_array = q_array.to_dataset(dim='variable')
+    q_array = q_array.assign_coords(percentile = np.linspace(0,1,201))
+    q_array = q_array.to_dataset(dim = 'variable')
     q_array = q_array.rename({0: 'correction_factors', 1: 'forecast_P_at_q', 2: 'obs_P_at_q'})
 
     print(q_array.correction_factors.values)
@@ -122,14 +134,13 @@ def Q_mapping_correction_factors(grid_frcst: Dataset, grid_clim_path):
         grid_frcst.P,
         distr,
         factors,
-        input_core_dims=[[], ['percentile'], ['percentile']],
-        output_core_dims=[['variable']],
+        input_core_dims=[[], ['percentile'],['percentile']],
+        output_core_dims = [['variable']],
         vectorize=True
     )
     return corrections
 
-
-def main_qq(derive_correction_factors=False):
+def main_qq(derive_correction_factors = False):
     start = time.perf_counter()
     work_dir = os.getcwd()
 
@@ -175,30 +186,32 @@ def main_qq(derive_correction_factors=False):
 
     # The actual work
     if derive_correction_factors:
-        for iMonth in range(3, 4):
+        for iMonth in range(3,4):
             if not os.path.exists(f'./intermediate/grid_quantile_correction_{iMonth}.nc'):
                 # try:
-                Q_mapping_gridref(iMonth, logger=logger, preprocess_files=True)
+                Q_mapping_gridref(iMonth, logger = logger, preprocess_files= True)
                 # except:
                 #     logger.warning('Could not compute correction factors in quantile mapping for month'
                 #                    f' index {iMonth}.')
+
 
     # Fetch the forecast
     forecast = xr.load_dataset('./input/forecast.nc')
     forecast = preprocess_FEWS_netCDF(forecast)
     month_int = forecast.analysis_time.dt.month.values[0]
-    forecast = check_dimensions(forecast, logger=logger)
+    forecast = check_dimensions(forecast, logger= logger)
 
-    forecast = forecast.isel({'analysis_time': forecast.analysis_time.dt.month == int(month_int)})  ## Select March
+
+    forecast = forecast.isel({'analysis_time': forecast.analysis_time.dt.month == int(month_int)}) ## Select March
     forecast['lon'] = np.round(forecast.lon.values, decimals=2)
     forecast['lat'] = np.round(forecast.lat.values, decimals=2)
 
     print('month: ', month_int)
     corrections = Q_mapping_correction_factors(forecast, f'./intermediate/grid_quantile_correction_{month_int}.nc')
     print(corrections)
-    corrections = corrections.to_dataset(dim='variable')
+    corrections = corrections.to_dataset(dim = 'variable')
     print(corrections)
-    corrections = corrections.rename({0: 'adjustment_factor', 1: 'P'})
+    corrections = corrections.rename({0: 'adjustment_factor', 1:'P'})
     print(corrections)
     corrections = corrections.swap_dims({'step': 'valid_time'})
     corrections = corrections.rename({'valid_time': 'time'})
@@ -206,7 +219,6 @@ def main_qq(derive_correction_factors=False):
     corrections.to_netcdf(f'./output/corrected_forecast.nc')
     return corrections
 
-
 if __name__ == '__main__':
     # For 1:12
-    main_qq(derive_correction_factors=True)
+    main_qq(derive_correction_factors= True)
